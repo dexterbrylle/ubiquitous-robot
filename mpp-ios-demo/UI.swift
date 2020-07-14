5cdaf591bd61d41c8976af2355c07970ccf56ccc
Dexter Brylle Matos <dexterbrylle@gmail.com>
2020-07-14 16:31:15 +0800

    Initial commit for Swift App

---
+++ mpp-ios-demo/UI.swift
@@ -0,0 +1,6 @@
+import Foundation
+import DEH_MPP
+//
+//public class UI: Kotlinx_coroutines_coreCoroutineDispatcher {
+//    override
+//}
--- mpp-ios-demo/ViewController.swift
+++ mpp-ios-demo/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import DEH_MPP
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class AccountsTableViewController: UITableViewController, AccountView {
+//    func showState(state: AccountState) {
+//        label = state.s
+//    }
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        let p = AccountsPresenter(view: self)
+//        p.start()
+//        self.label.text =
+//        self.label.text = SampleKt.hello()
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//    }
+//
+//    @IBOutlet weak var label: UILabel!
+    var accounts = [AccountsDisplay.Result]()
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return accounts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "AccountCellTableViewCell"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? AccountCellTableViewCell else {
+            fatalError("The dequeued cell is not an instance of AccountCellTableViewCell")
+        }
+        let account = accounts[indexPath.row]
+        cell.label.text = account.id
+        cell.account_name.text = account.account_name
+        return cell
+    }
 }
-
